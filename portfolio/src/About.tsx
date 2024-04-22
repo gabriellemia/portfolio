86344f2994bae92d4f397cea0da3480ff472b48c
gaby mia <78322726+gabriellemia@users.noreply.github.com>
2024-04-22 19:09:42 +0100

    sticky navbar and about adjustments

--- portfolio/src/About.tsx
+++ portfolio/src/About.tsx
@@ -4,11 +4,13 @@ import ProfileImage from "./ProfileImage";
 function About() {
   return (
     <>
-      <div className="profileImage">
-        <ProfileImage />
+      <div className="about-section">
+        <div className="profileImage">
+          <ProfileImage />
+        </div>
+        <h1 className="intro">Hi there, I'm Gaby!</h1>
+        <p className="about-text">A bit about me...</p>
       </div>
-      <h1 className="intro">Hi there, I'm Gaby!</h1>
-      <p className="about-section">A bit about me...</p>
     </>
   );
 }
